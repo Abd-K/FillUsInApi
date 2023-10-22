27eeda7dc267d18c669062f27093207725da45c5
abdul.khaled <abdul.khaled@vodafone.com>
2023-10-22 12:30:21 +0100

    fix security

--- src/main/java/org/fillUsIn/config/WebSecurityConfig.java
+++ src/main/java/org/fillUsIn/config/WebSecurityConfig.java
@@ -10,13 +10,17 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
+import java.util.List;
 
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
-    http.cors();  // other configurations
+    http.cors().and().csrf().disable()
+            .authorizeRequests()
+            .antMatchers("/**").permitAll()  // allow all requests
+            .anyRequest().authenticated();
   }
 
   @Bean
@@ -24,7 +28,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     CorsConfiguration configuration = new CorsConfiguration();
     configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "https://fillusin.com"));
     configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
-    configuration.setAllowedHeaders(Arrays.asList("*"));
+    configuration.setAllowedHeaders(List.of("*"));
     configuration.setAllowCredentials(true);
     UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
     source.registerCorsConfiguration("/**", configuration);
